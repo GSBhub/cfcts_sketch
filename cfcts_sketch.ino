32ebbbd98e22abce21caac450a8b3719c90cfd77
GSBhub <buthker.2@wright.edu>
2017-04-04 15:52:45 -0400

    Added some of David's changes
    Also worked on implementing the foundations for arduino code. However,
    the arduino-firebase library is not compatible with the UNO, so we need
    to make a design reconsideration

--- cfcts_sketch.ino
+++ cfcts_sketch.ino
@@ -1,30 +1,33 @@
+
+
 #include <SoftwareSerial.h> // MG2639 Lib uses SS Library
+#include <ESP8266wifi.h> // creating a frankenstein monster using ESP8266 wifi methods
 #include "Adafruit_FONA.h"  // Cell shield library include
-//#include "FirebaseArduino.h" // firebase library include - unstable, does not work
+#include "FirebaseArduino.h" // firebase library include - REQUIRES ESP8266 - DOES NOT WORK
+
+// Interrupt pin Initilization 
+#define HALT_PIN = 13; // 1 raises a HALT status in flight controller
 
 // adafruit FONA standard output PINs
 #define FONA_RX 2
 #define FONA_TX 3
-#define FONA_RST 4 
-
-#define FIREBASE_HOST "https://myfirstmapboxapp-11599.firebaseio.com/"
-#define FIREBASE_AUTH "token_or_secret"
-#define WIFI_SSID "SSID"
-#define WIFI_PASSWORD "PASSWORD"
+#define FONA_RST 4
+ 
+#define FIREBASE_HOST "myfirstmapboxapp-11599.firebaseio.com"
+#define FIREBASE_AUTH "jdi5ilRiQjD1QkT2zENBBOpex53NhqKBPyCNkMKO"
 
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX); // init the address 
 SoftwareSerial *fonaSerial = &fonaSS; // I'll have to look into this, this is from the test method
 
-// Interrupt pin Initilization 
-const int HALT_PIN = 13; // 1 raises a HALT status in flight controller
-
 //device init
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
-char url[80] = "https://myfirstmapboxapp-11599.firebaseio.com/"; // url for our firebase server
-
 //position data variables
 float latitude, longitude, speed_kph, heading, speed_mph, altitude;
+
+//device ID information
+char sim_id[21]; 
+char metadata_path[30];
 
 void setup() {
 
@@ -68,12 +71,38 @@ void setup() {
 
   //////////////////////////// END ADAFRUIT FONA NET SETUP ////////////////////////////////
 
+  //////////////////////////// BEGIN DEVICE ID SETUP //////////////////////////
+
+  // read the CCID (used w/ server to pull unique device ID information, potentially)
+  fona.getSIMCCID(sim_id);  // make sure sim_id is at least 21 bytes!
+  Serial.print(F("SIM CCID = ")); Serial.println(sim_id);
+
+  ///////////////////////////// END DEVICE ID SETUP ///////////////////////////
+
   //////////////////////////// BEGIN FIREBASE CONNECTION SETUP //////////////////////////
 
+  // connect to Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
+  char path[50] = "UserInfo/" + sim_id;
+
+  // check if SIMCCID is associtaed with an existing firebase identifier
+  if (Firebase.getString(path)){
+      char drone_name[20] = Firebase.getString(path);
+      Serial.print("UAS name found! \nName:");
+      Serial.print(drone_name + "\n");
+      metadata_path = "GeoFire/" + drone_name;
+    }
+   else {
+     Serial.print("No UAS associated with SIM " + sim_id ".");
+     while(1); 
+   }
+
+  Serial.println("Connection to Firebase successful!"); 
 
   ///////////////////////////// END FIREBASE CONNECTION SETUP ///////////////////////////
- 
+
+  
 }
  
 void loop() {
@@ -81,10 +110,13 @@ void loop() {
 /////////////PART 0: Server down, connection off////////
 //Display, allow some sort of error message? - TESTING
 
+if (Firebase.failed()){
+    Serial.print("Connection error with Firebase:")
+    Serial.println(Firebase.error());  
+}
 
-
-/////////////PART 1: Map Analysis///////////////////////
-//Determine WHERE you are, cast a ray to determine WHERE you will be
+/////////////PART 1: Location update///////////////////////
+//Determine WHERE you are
 
 if (fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude)){
   
@@ -101,15 +133,76 @@ if (fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude)){
     Serial.println(heading);
     Serial.print("GPS altitude:");
     Serial.println(altitude);
+
+/////////////PART 2: Update server location///////////////
+//Use firebase to send locational information to server
+
+    Firebase.setFloat(metadata_path + "/1/0", latitude);
+    Firebase.setFloat(metadata_path + "/1/1", longitude);
+    if (Firebase.success()){
+        Serial.println("Successful update of location information to Firebase!");
+      } else {
+      Serial.print("Error sending location to firebase:");
+      Serial.println(Firebase.error());    
+    }
+
+/////////////PART 3: Decision Making: NFZ///////////////
+//Implement No-fly zone logic, prevent flight in the forward direction
+
+     bool inNFZ = isThisPointInANoFlyZone(latitude, longitude);
+     if (inNFZ) {
+      
+        Serial.println("Detected entry into no-fly zone!"); 
+        HALT_PIN = 1; // send Return to sender signal to server
+     }
+
+     delay(6000);// wait for a few seconds
+    
   } else {
     Serial.println("Waiting for FONA GPS 3D fix...");
-    delay(30);
+    delay(300);
   }
 
 
-/////////////PART 2: Decision Making: NFZ///////////////
-//Implement No-fly zone logic, prevent flight in the forward direction
+}
 
-/////////////PART 3: Update and sync data///////////////
-//Upload current map data to the server, change NFZ as necessary
+  /*
+   * this will determine if at drone has entered a nofly zone
+   * it will get the no flyzones from firebase and the compare the 
+   * current point to each of the nofly zone polygons and determine 
+   * if the point is inside
+   * 
+   * Kudos to David Fay to writing these two methods
+   */
+bool isThisPointInANoFlyZone(double lat, double lng){
+
+  FirebaseObject noFlyZones = Firebase.get("NoFlyZones");
+  if (Firebase.success()){
+      Serial.println("Successfully got no fly zones!");
+      String data = noFlyZones.getString("zone1");
+      Serial.println(data);
+     }
+     if (Firebase.failed()){
+      Serial.print("get noflyzonse failed:");
+      Serial.println(Firebase.error());
+     }
+   
+  return false;
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
