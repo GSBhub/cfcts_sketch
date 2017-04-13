1315eab7b557d8733d9c6317fb694595a5e083ba
GSBhub <buthker.2@wright.edu>
2017-04-13 08:14:11 -0400

    Commited rest of David's code
    Note this commit goes to David, besides some compatibility issues.

--- cfcts_sketch.ino
+++ cfcts_sketch.ino
@@ -1,10 +1,12 @@
-#include <SoftwareSerial.h> // MG2639 Lib uses SS Library
 //#include <ESP8266WiFi.h>
 //#include <FirebaseArduino.h>
 #include "Adafruit_FONA.h"  // Cell shield library include
+#include <Servo.h> // servo library for our flight controller
+#include <SoftwareSerial.h> // MG2639 Lib uses SS Library
+
 
 // Interrupt pin Initilization 
-#define HALT_PIN 13 // 1 raises a HALT status in flight controller
+#define HALT_PIN 13 // PWM raises a HALT status in flight controller
 
 // adafruit FONA standard output PINs
 #define FONA_RX 3
@@ -14,13 +16,16 @@
 #define FIREBASE_HOST "myfirstmapboxapp-11599.firebaseio.com"
 #define FIREBASE_AUTH "jdi5ilRiQjD1QkT2zENBBOpex53NhqKBPyCNkMKO"
 
-SoftwareSerial fonaSerial = SoftwareSerial(FONA_TX, FONA_RX); // init the address 
-//SoftwareSerial *fonaSerial = &fonaSS; // I'll have to look into this, this is from the test method
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX); // init the address 
+SoftwareSerial *fonaSerial = &fonaSS; // I'll have to look into this, this is from the test method
 
 //HardwareSerial *fonaSerial = &Serial1;
 
 //device init
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+
+//define servo attach for flight controller
+Servo myservo;
 
 //position data variables
 float latitude, longitude, speed_kph, heading, speed_mph, altitude;
@@ -30,26 +35,26 @@ String metadata_path;
 
 void setup() {
 
-  Serial.println("Attempting to start serial communications...");
-
+  while (! Serial); // really, this seems to break it for some stupid reason
   Serial.begin(115200); // this is the standard baud for our cell shield
 
   // halt until serial up (active wait) -> could be passive, potentially
-   while (! Serial); // really, this seems to break it for some stupid reason
 
   Serial.println("Serial communicaitons started.");
 
   //Init network serial communications
   // set up interrupt pin
-//  pinMode(HALT_PIN, OUTPUT); 
+  
+  // this is the pin that the pwm signal will be sent on :
+  myservo.attach(HALT_PIN);
 
   ///////////////////////////// BEGIN ADAFRUIT FONA NET SETUP //////////////////////////////
 
   Serial.println("Attempting to start the FONA...");
 
-  fonaSerial.begin(9600); 
-  
-  if (! fona.begin(fonaSerial)) {
+  fonaSerial->begin(4800); 
+
+  if (! fona.begin(*fonaSerial)) {
     Serial.println(F("Couldn't find FONA"));
     while(1);
   }
@@ -164,16 +169,17 @@ if (fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude)){
     }
 */
 
-  String NFZ = ""; // Hardcoded NFZ from firebase, cannot access firebase
 
 /////////////PART 3: Decision Making: NFZ///////////////
 //Implement No-fly zone logic, prevent flight in the forward direction
 
      bool inNFZ = isThisPointInANoFlyZone(latitude, longitude);
      if (inNFZ) {
-      
         Serial.println("Detected entry into no-fly zone!"); 
-        analogWrite(HALT_PIN, 128); // send Return to sender signal to server (128 is a test PWM for now)
+        // the is the pwm signal
+        // put this in the section after the drone is dectected in the
+        // NFZ and that is that
+        myservo.writeMicroseconds(1800);        
         while(1);
      }
 
@@ -193,26 +199,122 @@ if (fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude)){
    * current point to each of the nofly zone polygons and determine 
    * if the point is inside
    * 
-   * Kudos to David Fay to writing these two methods
+   * Kudos to David Fay to writing everything below this point ---------------------------------------------
    */
-bool isThisPointInANoFlyZone(double lat, double lng){
+//
+//float createSteps(float a, float b, int number_of_steps){
+// /*
+//  * calculates the proper incriment between to vectors
+//  */
+//  
+//  float delta = b - a;
+//  float incriment = delta/number_of_steps;
+//  return incriment;
+//  
+//}
+//
+//void flyLeg(float start_lat, float start_lng, float stop_lat,  float stop_lng, int number_of_steps){
+//  /*
+//   * simulates a drone transmiting its locaion to our database by taking two sets of corordinates
+//   * and filling in the path between them with intermediate points.  
+//   */
+//
+//  // calculate how far to step between each point 
+//  // aka divied the distance between each point by the number of steps
+//  float lat_step = createSteps(start_lat, stop_lat, number_of_steps);
+//  float lng_step = createSteps(start_lng, stop_lng, number_of_steps);
+//  float each_lat = start_lat;
+//  float each_lng = start_lng;
+//
+//  // get to step'n
+//  int i;
+//  for (i = 0; i<number_of_steps; i = i+1){
+//     each_lat = each_lat + lat_step;
+//     each_lng = each_lng + lng_step;
+//     // send the current locatin to the database
+//     // TODO change this to transmit both points simultaniously
+//     Firebase.setFloat("GeoFire/Rouge One/l/0", each_lat);
+//     Firebase.setFloat("GeoFire/Rouge One/l/1", each_lng);
+//
+//      // Serial.println("Successfully updated Location!");
+//      bool inNFZ = isThisPointInANoFlyZone(each_lat, each_lng);
+//      if(inNFZ){
+//        Serial.println("inside nofly zone");
+//      }else{
+//        Serial.println("not inside nofly zone");
+//      }
+//     
+//     delay(100); 
+////  }
+////}
 
-  FirebaseObject noFlyZones = Firebase.get("NoFlyZones");
-  if (Firebase.success()){
-      Serial.println("Successfully got no fly zones!");
-      String data = noFlyZones.getString("zone1");
-      Serial.println(data);
-     }
-     if (Firebase.failed()){
-      Serial.print("get noflyzonse failed:");
-      Serial.println(Firebase.error());
-     }
-   
-  return false;
+bool pointInsidePolygon(String polygonlist, float lat, float lng){
+  /*
+   * Determine if the lat/lng location in inside the polygon represented by 
+   * the polygon list saved in kml format (lng,lat,alt).
+   */
+
+  int num_o_spaces = countSpaces(polygonlist);
+  char space = 32;
+  bool inside = false;
+  
+  // iterate over all of the points in the polygonlist string from the database      
+  for(int i=0; i <= num_o_spaces; i++){
+    // get the lng,lat,alt entry at position i from the polygon string
+    String current_point = getValue(polygonlist, space, i);
+    // get the lng entry from the locaion string at poisition i 
+    String p1_lat_str = getValue(current_point, ',', 1);
+    float p1_lat = p1_lat_str.toFloat();
+    // get the lat entry from the location string at poisition i 
+    String p1_lng_str = getValue(current_point, ',', 0);
+    float p1_lng = p1_lng_str.toFloat();
+    
+    // do it all again for the next point to create a line between them
+    String next_point = getValue(polygonlist, space, (i+1)%num_o_spaces);
+    // lng
+    String p2_lat_str = getValue(next_point, ',', 1);
+    float p2_lat = p2_lat_str.toFloat();
+    // lat
+    String p2_lng_str = getValue(next_point, ',', 0);
+    float p2_lng = p2_lng_str.toFloat();
+   // check if a ray cast from this point intersects the edge of this polygon 
+    inside = lineIntersect(inside, lng, lat, p1_lng, p1_lat, p2_lng, p2_lat);
+  }
+  return inside;       
 }
 
-String getValue(String data, char separator, int index)
-{
+bool lineIntersect(bool inside, const float x, const float y, const float p1_x, const float p1_y, const float p2_x, const float p2_y){
+  /*
+   * Cast a line from the point at x,y and determine if it intersects with a line beteween 
+   * p1_x,p1_y and p2_x,p2_y
+   * 
+   */
+   
+  float min_y = min(p1_y, p2_y);
+  if (y > min_y){
+    float max_y = max(p1_y, p2_y);
+    if (y <= max_y){
+      float max_x = max(p1_x, p2_x);
+      if(x <= max_x){
+        if(p1_y != p2_y){
+          float xinters = (y-p1_y)*(p2_x-p1_x)/(p2_y-p1_y)+ p1_x;
+          if((p1_x==p2_x)or(x<=xinters)){
+            inside = !inside; 
+          }
+        }
+      }
+    }
+  }
+  return inside;
+}
+
+String getValue(String data, char separator, int index){
+  /*
+   * Split a string by the characeter passed in seperator 
+   * and get the section between the two seporators at the 
+   * position specified by index.
+   */
+   
   int found = 0;
   int strIndex[] = {0, -1};
   int maxIndex = data.length()-1;
@@ -228,3 +330,191 @@ String getValue(String data, char separator, int index)
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+int countSpaces(String string){
+  /*
+   * Count how many spaces are in a string.
+   * This will be used to determine how many points are in 
+   * the polygonlist strings.  
+   */
+
+  int number_of_spaces = 0;
+  int i;
+  char space = 32;
+  
+  for (i = 0; i < string.length(); i++){
+    char this_char = string[i];
+//    Serial.println(this_char);
+    if (this_char == space){
+      number_of_spaces++;
+    }
+  }
+  return number_of_spaces;
+}
+
+bool isThisPointInANoFlyZone(double lat, double lng){
+  /*
+   * Determine if this drone has entered any of the nofly zones
+   * from our database.
+   * 1. Get the no flyzones from firebase. 
+   * 2. Compare the current location to each of the nofly zone polygons 
+   * and determine if the point is inside.  
+   */
+
+  // get all of the no-fly zones 
+  //FirebaseObject noFlyZones = Firebase.get("NoFlyZones");
+  // check for success response  
+
+    // get the number of no fly-zones from the first entry in the datbase
+    int num_o_zones = 1; // only hardcoding one zone for now
+    char space = 32;
+    // start iterating through all the no-flyzones
+    for(int z=0; z<num_o_zones; z++){
+      char zone_num = (z+49);
+      String zone = "zone_";
+      String zone_key = zone+zone_num;
+      String polygonlist = "-83.74171167612076,39.69924181876981,0 -83.74193698167801,39.69910148613672,0 -83.74302595853806,39.700046662183496,0 -83.74256998300552,39.700306686511084,0 -83.74171167612076,39.69924181876981,0";
+      // check if the current location is in this no-fly zone
+      if(pointInsidePolygon(polygonlist, lat, lng)){
+        return true;
+      }
+    }
+
+  return false;
+}
+
+//bool isThisPointInANoFlyZone(double lat, double lng){
+//  /*
+//   * Determine if this drone has entered any of the nofly zones
+//   * from our database.
+//   * 1. Get the no flyzones from firebase. 
+//   * 2. Compare the current location to each of the nofly zone polygons 
+//   * and determine if the point is inside.  
+//   */
+//
+//  // get all of the no-fly zones 
+//  // FIREBASE BROKEN, DEFINE STRINGS
+//  // FirebaseObject noFlyZones = Firebase.get("NoFlyZones");
+//  // check for success response  
+//
+//
+//
+//    // get the number of no fly-zones from the first entry in the datbase
+//    int num_o_zones = 1; // Only hardcoding one zone for now
+//    char space = 32;
+//    // start iterating through all the no-flyzones
+//    for(int z=0; z<num_o_zones; z++){
+//      char zone_num = (z+49);
+//      String zone = "zone_";
+//      String zone_key = zone+zone_num;
+//      String polygonlist = noFlyZones.getString(zone_key);
+//      // check if the current location is in this no-fly zone
+//      if(pointInsidePolygon(polygonlist, lat, lng)){
+//        return true;
+//      }
+//    }
+//
+//  return false;
+//}
+//
+//bool pointInsidePolygon(String polygonlist, float lat, float lng){
+//  /*
+//   * Determine if the lat/lng location in inside the polygon represented by 
+//   * the polygon list saved in kml format (lng,lat,alt).
+//   */
+//
+//  int num_o_spaces = countSpaces(polygonlist);
+//  char space = 32;
+//  bool inside = false;
+//  
+//  // iterate over all of the points in the polygonlist string from the database      
+//  for(int i=0; i <= num_o_spaces; i++){
+//    // get the lng,lat,alt entry at position i from the polygon string
+//    String current_point = getValue(polygonlist, space, i);
+//    // get the lng entry from the locaion string at poisition i 
+//    String p1_lat_str = getValue(current_point, ',', 1);
+//    float p1_lat = p1_lat_str.toFloat();
+//    // get the lat entry from the location string at poisition i 
+//    String p1_lng_str = getValue(current_point, ',', 0);
+//    float p1_lng = p1_lng_str.toFloat();
+//    
+//    // do it all again for the next point to create a line between them
+//    String next_point = getValue(polygonlist, space, (i+1)%num_o_spaces);
+//    // lng
+//    String p2_lat_str = getValue(next_point, ',', 1);
+//    float p2_lat = p2_lat_str.toFloat();
+//    // lat
+//    String p2_lng_str = getValue(next_point, ',', 0);
+//    float p2_lng = p2_lng_str.toFloat();
+//   // check if a ray cast from this point intersects the edge of this polygon 
+//    inside = lineIntersect(inside, lng, lat, p1_lng, p1_lat, p2_lng, p2_lat);
+//  }
+//  return inside;       
+//}
+//
+//bool lineIntersect(bool inside, const float x, const float y, const float p1_x, const float p1_y, const float p2_x, const float p2_y){
+//  /*
+//   * Cast a line from the point at x,y and determine if it intersects with a line beteween 
+//   * p1_x,p1_y and p2_x,p2_y
+//   * 
+//   */
+//   
+//  float min_y = std::min(p1_y, p2_y);
+//  if (y > min_y){
+//    float max_y = std::max(p1_y, p2_y);
+//    if (y <= max_y){
+//      float max_x = std::max(p1_x, p2_x);
+//      if(x <= max_x){
+//        if(p1_y != p2_y){
+//          float xinters = (y-p1_y)*(p2_x-p1_x)/(p2_y-p1_y)+ p1_x;
+//          if((p1_x==p2_x)or(x<=xinters)){
+//            inside = !inside; 
+//          }
+//        }
+//      }
+//    }
+//  }
+//  return inside;
+//}
+//
+//String getValue(String data, char separator, int index){
+//  /*
+//   * Split a string by the characeter passed in seperator 
+//   * and get the section between the two seporators at the 
+//   * position specified by index.
+//   */
+//   
+//  int found = 0;
+//  int strIndex[] = {0, -1};
+//  int maxIndex = data.length()-1;
+//
+//  for(int i=0; i<=maxIndex && found<=index; i++){
+//    if(data.charAt(i)==separator || i==maxIndex){
+//        found++;
+//        strIndex[0] = strIndex[1]+1;
+//        strIndex[1] = (i == maxIndex) ? i+1 : i;
+//    }
+//  }
+//
+//  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+//}
+//
+//int countSpaces(String string){
+//  /*
+//   * Count how many spaces are in a string.
+//   * This will be used to determine how many points are in 
+//   * the polygonlist strings.  
+//   */
+//
+//  int number_of_spaces = 0;
+//  int i;
+//  char space = 32;
+//  
+//  for (i = 0; i < string.length(); i++){
+//    char this_char = string[i];
+////    Serial.println(this_char);
+//    if (this_char == space){
+//      number_of_spaces++;
+//    }
+//  }
+//  return number_of_spaces;
+//}
